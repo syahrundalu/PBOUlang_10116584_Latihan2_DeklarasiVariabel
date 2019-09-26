5f94fa47519201d92f0f8a3a4b45a2b2ffafd625
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-09-26 13:43:44 +0700

    Add files via upload

---
+++ PBOUlang_10116584_Latihan2_DeklarasiVariabel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan2_DeklarasiVariabel;
+
+/**
+ *
+ * @author
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class PBOUlang_10116584_Latihan2_DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     // Deklarasi Variabel
+     int Nilaiint;
+     final double PHI = 3.14; //konstanta
+     boolean nilaiLogika;
+     char nilaiKarakter;
+     
+     //memberi Nilai Ke variabel
+     Nilaiint = 78;
+     nilaiLogika = false;
+     nilaiKarakter = 'D';
+     
+     //Menampilkan Hasil
+        System.out.println("");
+        System.out.println("Isi Variabel Nilai    = "+ Nilaiint);
+        System.out.println("Isi Nilai PHI         = "+ PHI);
+        System.out.println("Isi Nilai Logika      = "+ nilaiLogika);
+        System.out.println("Isi Variabel Karakter = "+ nilaiKarakter);
+             
+    }
+    
+}
